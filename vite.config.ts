94510982e8163bb9ed900910eb2ecb8427d3f25e
K.K <enrich.k.kobayashi@gmail.com>
2026-01-24 21:30:28 +0900

    GitHub Pages用の設定を追加

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
   return {
+    base: '/my_projyect-moni/',
     server: {
       port: 3000,
       strictPort: true,
