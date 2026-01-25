5df857d441190eda88f7166a942feaa25ecc778f
K.K <enrich.k.kobayashi@gmail.com>
2026-01-25 10:04:14 +0900

    Vercelデプロイ用に設定を変更

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
   return {
-    base: '/my_project-moni/',
+
     server: {
       port: 3000,
       strictPort: true,
